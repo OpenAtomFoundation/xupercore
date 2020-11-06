ef047a452180f891384f620cefb3eda94754e99c
ownluke <ownluke@gmail.com>
2020-11-06 15:25:41 +0800

    network

--- kernel/network/config/config.go
+++ kernel/network/config/config.go
@@ -1,125 +1,135 @@
 package config
 
 import (
-	"fmt"
-
-	"github.com/xuperchain/xupercore/kernel/common/xutils"
-	"github.com/xuperchain/xupercore/lib/utils"
-
-	"github.com/spf13/viper"
+    "fmt"
+    "github.com/spf13/viper"
+    utils "github.com/xuperchain/xupercore/kernel/common/xutils"
 )
 
-type P2PConfig struct {
-	// port the p2p network listened
-	Port int32 `yaml:"port,omitempty"`
-	// keyPath is the node private key path, xuper will gen a random one if is nil
-	KeyPath string `yaml:"keyPath,omitempty"`
-	// isNat config whether the node use NAT manager
-	IsNat bool `yaml:"isNat,omitempty"`
-	// isSecure config whether the node use secure transparent
-	IsSecure bool `yaml:"isSecure,omitempty"`
-	// isHidden config whether the node can be found
-	IsHidden bool `yaml:"isHidden,omitempty"`
-	// bootNodes config the bootNodes the node to connect
-	BootNodes []string `yaml:"bootNodes,omitempty"`
-	// staticNodes config the nodes which you trust
-	StaticNodes map[string][]string `yaml:"staticNodes,omitempty"`
-	// isBroadCast config whether broadcast to all StaticNodes
-	IsBroadCast bool `yaml:"isBroadCast,omitempty"`
-	// maxStreamLimits config the max stream num
-	MaxStreamLimits int32 `yaml:"maxStreamLimits,omitempty"`
-	// maxMessageSize config the max message size
-	MaxMessageSize int64 `yaml:"maxMessageSize,omitempty"`
-	// timeout config the timeout of Request with response
-	Timeout int64 `yaml:"timeout,omitempty"`
-	// IsAuthentication determine whether peerID and Xchain addr correspond
-	IsAuthentication bool `yaml:"isauthentication,omitempty"`
-	// StreamIPLimitSize set the limitation size for same ip
-	StreamIPLimitSize int64 `yaml:"streamIPLimitSize,omitempty"`
-	// MaxBroadcastPeers limit the number of common peers in a broadcast,
-	// this number do not include MaxBroadcastCorePeers.
-	MaxBroadcastPeers int `yaml:"maxBroadcastPeers,omitempty"`
-	// MaxBroadcastCorePeers limit the number of core peers in a broadcast,
-	// this only works when NodeConfig.CoreConnection is true. Note that the number
-	// of core peers is included in MaxBroadcastPeers.
-	MaxBroadcastCorePeers int `yaml:"maxBroadcastCorePeers,omitempty"`
-	// P2PDataPath stores the peer info connected last time
-	P2PDataPath string `yaml:"p2PDataPath,omitempty"`
-	// IsStorePeers determine wherther storing the peers infos
-	IsStorePeers bool `yaml:"isStorePeers,omitempty"`
-	// CertPath define the path of certificate
-	CertPath string `yaml:"certPath,omitempty"`
-	// IsUseCert define whether to use certificate, default true
-	IsUseCert bool `yaml:"isUseCert,omitempty"`
-	// ServiceName
-	ServiceName string `yaml:"serviceName,omitempty"`
+// default settings
+const (
+    DefaultNetPort          = 47101             // p2p port
+    DefaultNetKeyPath       = "./data/netkeys" 	// node private key path
+    DefaultNetIsNat         = true  			// use NAT
+    DefaultNetIsTls         = false             // use tls secure transport
+    DefaultNetIsHidden      = false
+    DefaultNetIsIpv6        = false
+    DefaultMaxStreamLimits  = 1024
+    DefaultMaxMessageSize   = 128
+    DefaultTimeout          = 3
+    DefaultStreamIPLimitSize     = 10
+    DefaultMaxBroadcastPeers     = 20
+    DefaultIsStorePeers          = false
+    DefaultP2PDataPath           = "./data/p2p"
+    DefaultP2PModuleName         = "p2pv2"
+    DefaultServiceName           = "localhost"
+    DefaultIsBroadCast           = true
+)
+
+// Config is the config of p2p server. Attention, config of dht are not expose
+type Config struct {
+    // Module is the name of p2p module plugin
+    Module string `yaml:"module,omitempty"`
+    // port the p2p network listened
+    Port int32 `yaml:"port,omitempty"`
+    // keyPath is the node private key path, xuper will gen a random one if is nil
+    KeyPath string `yaml:"keyPath,omitempty"`
+    // isNat config whether the node use NAT manager
+    IsNat bool `yaml:"isNat,omitempty"`
+    // isHidden config whether the node can be found
+    IsHidden bool `yaml:"isHidden,omitempty"`
+    // IsIpv6 config whether the node use ipv6
+    IsIpv6 bool `yaml:"isIpv6,omitempty"`
+    // bootNodes config the bootNodes the node to connect
+    BootNodes []string `yaml:"bootNodes,omitempty"`
+    // staticNodes config the nodes which you trust
+    StaticNodes map[string][]string `yaml:"staticNodes,omitempty"`
+    // isBroadCast config whether broadcast to all StaticNodes
+    IsBroadCast bool `yaml:"isBroadCast,omitempty"`
+    // maxStreamLimits config the max stream num
+    MaxStreamLimits int32 `yaml:"maxStreamLimits,omitempty"`
+    // maxMessageSize config the max message size
+    MaxMessageSize int64 `yaml:"maxMessageSize,omitempty"`
+    // timeout config the timeout of Request with response
+    Timeout int64 `yaml:"timeout,omitempty"`
+    // StreamIPLimitSize set the limitation size for same ip
+    StreamIPLimitSize int64 `yaml:"streamIPLimitSize,omitempty"`
+    // MaxBroadcastPeers limit the number of common peers in a broadcast,
+    // this number do not include MaxBroadcastCorePeers.
+    MaxBroadcastPeers int `yaml:"maxBroadcastPeers,omitempty"`
+    // IsStorePeers determine wherther storing the peers infos
+    IsStorePeers bool `yaml:"isStorePeers,omitempty"`
+    // P2PDataPath stores the peer info connected last time
+    P2PDataPath string `yaml:"p2PDataPath,omitempty"`
+    // isTls config the node use tls secure transparent
+    IsTls bool `yaml:"isTls,omitempty"`
+    // ServiceName
+    ServiceName string `yaml:"serviceName,omitempty"`
 }
 
-func LoadP2PConf(cfgFile string) (*P2PConfig, error) {
-	cfg := GetDefP2PConf()
-	err := cfg.loadConf(cfgFile)
-	if err != nil {
-		return nil, fmt.Errorf("load p2p config failed.err:%s", err)
-	}
+func LoadP2PConf(cfgFile string) (*Config, error) {
+    cfg := GetDefP2PConf()
+    err := cfg.loadConf(cfgFile)
+    if err != nil {
+        return nil, fmt.Errorf("load p2p config failed.err:%s", err)
+    }
 
-	return cfg, nil
+    return cfg, nil
 }
 
-func GetDefP2PConf() *P2PConfig {
-	return &P2PConfig{
-		Port:             47101,
-		KeyPath:          "./data/netkeys/",
-		IsNat:            true,
-		IsSecure:         true,
-		IsHidden:         false,
-		MaxStreamLimits:  1024,
-		MaxMessageSize:   128,
-		Timeout:          3,
-		IsAuthentication: false,
-		// default stream ip limit size
-		StreamIPLimitSize:     10,
-		MaxBroadcastPeers:     20,
-		MaxBroadcastCorePeers: 17,
-		IsStorePeers:          false,
-		P2PDataPath:           "./data/p2p",
-		StaticNodes:           make(map[string][]string),
-		CertPath:              "./data/cert",
-		IsUseCert:             true,
-		ServiceName:           "",
-		IsBroadCast:           true,
-	}
+func GetDefP2PConf() *Config {
+    return &Config{
+        Module:           DefaultP2PModuleName,
+        Port:             DefaultNetPort,
+        KeyPath:          DefaultNetKeyPath,
+        IsNat:            DefaultNetIsNat,
+        IsTls:            DefaultNetIsTls,
+        IsHidden:         DefaultNetIsHidden,
+        IsIpv6:           DefaultNetIsIpv6,
+        MaxStreamLimits:  DefaultMaxStreamLimits,
+        MaxMessageSize:   DefaultMaxMessageSize,
+        Timeout:          DefaultTimeout,
+        // default stream ip limit size
+        StreamIPLimitSize:     DefaultStreamIPLimitSize,
+        MaxBroadcastPeers:     DefaultMaxBroadcastPeers,
+        IsStorePeers:          DefaultIsStorePeers,
+        P2PDataPath:           DefaultP2PDataPath,
+        StaticNodes:           make(map[string][]string),
+        ServiceName:           DefaultServiceName,
+        IsBroadCast:           DefaultIsBroadCast,
+    }
 }
 
-func (t *P2PConfig) loadConf(cfgFile string) error {
-	if cfgFile == "" || !utils.FileIsExist(cfgFile) {
-		return fmt.Errorf("config file set error.path:%s", cfgFile)
-	}
+func (t *Config) loadConf(cfgFile string) error {
+    if cfgFile == "" {
+        return fmt.Errorf("config file set error.path:%s", cfgFile)
+    }
 
-	viperObj := viper.New()
-	viperObj.SetConfigFile(cfgFile)
-	err := viperObj.ReadInConfig()
-	if err != nil {
-		return fmt.Errorf("read config failed.path:%s,err:%v", cfgFile, err)
-	}
+    viperObj := viper.New()
+    viperObj.SetConfigFile(cfgFile)
+    err := viperObj.ReadInConfig()
+    if err != nil {
+        return fmt.Errorf("read config failed.path:%s,err:%v", cfgFile, err)
+    }
 
-	if err = viperObj.Unmarshal(t); err != nil {
-		return fmt.Errorf("unmatshal config failed.path:%s,err:%v", cfgFile, err)
-	}
+    if err = viperObj.Unmarshal(t); err != nil {
+        return fmt.Errorf("unmatshal config failed.path:%s,err:%v", cfgFile, err)
+    }
 
-	return nil
+    return nil
 }
 
 // For testing.
 const (
-	DefConfFile = "conf/network.yaml"
+    DefConfFile = "conf/network.yaml"
 )
 
 // For testing.
-// 为了方便单测运行，设置一个环境变量X_ROOT_PATH，统一从这个目标加载配置和数据
+// 为了方便单测运行，设置一个环境变量XCHAIN_UT_PATH，统一从这个目标加载配置和数据
 func GetNetConfFile() string {
-	var utPath = xutils.GetXRootPath()
-	if utPath == "" {
-		panic("X_ROOT_PATH environment variable not set")
-	}
-	return utPath + DefConfFile
+    var utPath = utils.GetXRootPath()
+    if utPath == "" {
+        panic("XCHAIN_ROOT_PATH environment variable not set")
+    }
+    return utPath + DefConfFile
 }
--- kernel/network/network.go
+++ kernel/network/network.go
@@ -5,24 +5,24 @@ import (
 	"sort"
 	"sync"
 
-	xctx "github.com/xuperchain/xupercore/kernel/common/xcontext"
-	nbase "github.com/xuperchain/xupercore/kernel/network/base"
 	nctx "github.com/xuperchain/xupercore/kernel/network/context"
+	"github.com/xuperchain/xupercore/kernel/network/p2p"
+	pb "github.com/xuperchain/xupercore/kernel/network/pb"
 )
 
 // 创建P2PServer实例方法
-type NewP2PServFunc func() Network
+type NewP2PServFunc func() p2p.Server
 
 var (
-	servsMu  sync.RWMutex
+	servMu  sync.RWMutex
 	services = make(map[string]NewP2PServFunc)
 )
 
 // Register makes a driver available by the provided name.
 // If Register is called twice with the same name or if driver is nil,it panics.
 func Register(name string, f NewP2PServFunc) {
-	servsMu.Lock()
-	defer servsMu.Unlock()
+	servMu.Lock()
+	defer servMu.Unlock()
 
 	if f == nil {
 		panic("network: Register new func is nil")
@@ -35,8 +35,8 @@ func Register(name string, f NewP2PServFunc) {
 
 // Drivers returns a sorted list of the names of the registered drivers.
 func Drivers() []string {
-	servsMu.RLock()
-	defer servsMu.RUnlock()
+	servMu.RLock()
+	defer servMu.RUnlock()
 	list := make([]string, 0, len(services))
 	for name := range services {
 		list = append(list, name)
@@ -45,9 +45,9 @@ func Drivers() []string {
 	return list
 }
 
-func createP2PServ(name string) nbase.P2PServer {
-	servsMu.RLock()
-	defer servsMu.RUnlock()
+func createP2PServ(name string) p2p.Server {
+	servMu.RLock()
+	defer servMu.RUnlock()
 
 	if f, ok := services[name]; ok {
 		return f()
@@ -58,33 +58,26 @@ func createP2PServ(name string) nbase.P2PServer {
 
 // network对外提供的接口
 type Network interface {
-	// send msg
-	SendMsg(ctx *xctx.BaseCtx, msg *xuperp2p.XuperMessage, opts ...MessageOption) error
-	SendMsgWithResp(ctx *xctx.BaseCtx, msg *xuperp2p.XuperMessage,
-		opts ...MessageOption) ([]*xuperp2p.XuperMessage, error)
-	// query info
-	GetNetURL() string
-	GetPeerUrls() []string
-	GetPeerIDAndUrls() map[string]string
-	SetCorePeers(cp *CorePeersInfo) error
-	SetXchainAddr(bcname string, info *XchainAddrInfo)
-	// subscriber msg
-	NewSubscriber(msgChan chan *xuperp2p.XuperMessage, msgType xuperp2p.XuperMessage_MessageType,
-		handle XuperHandler, msgFrom string, octx nctx.ObjCtx) (Subscriber, error)
-	Register(sub Subscriber) (Subscriber, error)
-	UnRegister(sub Subscriber) error
+	SendMessage(nctx.OperateCtx, *pb.XuperMessage, ...p2p.OptionFunc) error
+	SendMessageWithResponse(nctx.OperateCtx, *pb.XuperMessage, ...p2p.OptionFunc) ([]*pb.XuperMessage, error)
+
+	//NewSubscriber(context.Context, pb.XuperMessage_MessageType, ...SubscriberOption) Subscriber
+	Register(p2p.Subscriber) error
+	UnRegister(p2p.Subscriber) error
+
+	P2PState(nctx.OperateCtx) p2p.State
 }
 
 // 如果有领域内公共逻辑，可以在这层扩展，对上层暴露高级接口
 // 暂时没有特殊的逻辑，先简单透传，预留方便后续扩展
 type NetworkImpl struct {
-	netCtx  *nctx.NetCtx
-	p2pServ nbase.P2PServer
+	ctx    nctx.DomainCtx
+	p2pServ p2p.Server
 }
 
-func CreateNetwork(servName string, netCtx *nctx.NetCtx) (Network, error) {
+func CreateNetwork(ctx nctx.DomainCtx, servName string) (Network, error) {
 	// check param
-	if octx == nil || !octx.IsVaild() {
+	if ctx == nil || !ctx.IsValid() {
 		return nil, fmt.Errorf("new network failed because context set error")
 	}
 
@@ -94,88 +87,33 @@ func CreateNetwork(servName string, netCtx *nctx.NetCtx) (Network, error) {
 		return nil, fmt.Errorf("new network failed because service not exist.name:%s", servName)
 	}
 	// init p2p server
-	err := p2pServ.Init(netCtx)
+	err := p2pServ.Init(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("new network failed because init p2p service error.err:%v", err)
 	}
 	// start p2p server
 	p2pServ.Start()
 
-	return &NetworkImpl{netCtx, p2pServ}, nil
+	return &NetworkImpl{ctx, p2pServ}, nil
 }
 
-func (t *NetworkImpl) SendMsg(ctx *xctx.BaseCtx, msg *xuperp2p.XuperMessage,
-	opts ...MessageOption) error {
-
+func (t *NetworkImpl) SendMessage(ctx nctx.OperateCtx, msg *pb.XuperMessage, opts ...p2p.OptionFunc) error {
 	if !t.isInit() || ctx == nil || msg == nil {
 		return fmt.Errorf("network not init or param set error")
 	}
 
-	return t.p2pServ.SendMessage(ctx, msg, opts)
+	return t.p2pServ.SendMessage(ctx, msg, opts...)
 }
 
-func (t *NetworkImpl) SendMsgWithResp(ctx *xctx.BaseCtx, msg *xuperp2p.XuperMessage,
-	opts ...MessageOption) ([]*xuperp2p.XuperMessage, error) {
-
+func (t *NetworkImpl) SendMessageWithResponse(ctx nctx.OperateCtx, msg *pb.XuperMessage, opts ...p2p.OptionFunc) ([]*pb.XuperMessage, error) {
 	if !t.isInit() || ctx == nil || msg == nil {
-		return fmt.Errorf("network not init or param set error")
+		return nil, fmt.Errorf("network not init or param set error")
 	}
 
-	return t.p2pServ.SendMessageWithResponse(ctx, msg, opts)
+	return t.p2pServ.SendMessageWithResponse(ctx, msg, opts...)
 }
 
-func (t *NetworkImpl) GetNetURL() string {
-	if !t.isInit() {
-		return fmt.Errorf("network not init or param set error")
-	}
-
-	return t.p2pServ.GetNetURL()
-}
-
-func (t *NetworkImpl) GetPeerUrls() []string {
-	if !t.isInit() {
-		return fmt.Errorf("network not init or param set error")
-	}
-
-	return t.p2pServ.GetPeerUrls()
-}
-
-func (t *NetworkImpl) GetPeerIDAndUrls() map[string]string {
-	if !t.isInit() {
-		return fmt.Errorf("network not init or param set error")
-	}
-
-	return t.p2pServ.GetPeerIDAndUrls()
-}
-
-func (t *NetworkImpl) SetCorePeers(cp *CorePeersInfo) error {
-	if !t.isInit() || cp == nil {
-		return fmt.Errorf("network not init or param set error")
-	}
-
-	return t.p2pServ.SetCorePeers(cp)
-}
-
-func (t *NetworkImpl) SetXchainAddr(bcname string, info *XchainAddrInfo) {
-	if !t.isInit() || bcname == "" || info == nil {
-		return fmt.Errorf("network not init or param set error")
-	}
-
-	return t.p2pServ.SetXchainAddr(bcname, info)
-}
-
-func (t *NetworkImpl) NewSubscriber(msgChan chan *xuperp2p.XuperMessage,
-	msgType xuperp2p.XuperMessage_MessageType, handle XuperHandler, msgFrom string,
-	netCtx *nctx.NetCtx) (Subscriber, error) {
-
-	if !t.isInit() || msgChan == nil || netCtx == nil {
-		return fmt.Errorf("network not init or param set error")
-	}
-
-	return t.p2pServ.NewSubscriber(msgChan, msgType, handle, msgFrom, netCtx)
-}
-
-func (t *NetworkImpl) Register(sub Subscriber) (Subscriber, error) {
+func (t *NetworkImpl) Register(sub p2p.Subscriber) error {
 	if !t.isInit() || sub == nil {
 		return fmt.Errorf("network not init or param set error")
 	}
@@ -183,7 +121,7 @@ func (t *NetworkImpl) Register(sub Subscriber) (Subscriber, error) {
 	return t.p2pServ.Register(sub)
 }
 
-func (t *NetworkImpl) UnRegister(sub Subscriber) error {
+func (t *NetworkImpl) UnRegister(sub p2p.Subscriber) error {
 	if !t.isInit() || sub == nil {
 		return fmt.Errorf("network not init or param set error")
 	}
@@ -191,8 +129,12 @@ func (t *NetworkImpl) UnRegister(sub Subscriber) error {
 	return t.p2pServ.UnRegister(sub)
 }
 
+func (t *NetworkImpl) P2PState(nctx.OperateCtx) p2p.State {
+	return p2p.State{}
+}
+
 func (t *NetworkImpl) isInit() bool {
-	if t.netCtx == nil || t.p2pServ == nil {
+	if t.ctx == nil || t.p2pServ == nil {
 		return false
 	}
 
