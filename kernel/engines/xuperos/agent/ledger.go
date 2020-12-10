7c342c8fca4da554d57666ab85917a800aed951a
chunhui01 <chunhui01@outlook.com>
2020-12-10 19:27:24 +0800

    xuperos:add ledger agent

--- kernel/engines/xuperos/agent/ledger.go
+++ kernel/engines/xuperos/agent/ledger.go
@@ -1,6 +1,8 @@
 package agent
 
 import (
+	"github.com/xuperchai/xupercore/kernel/ledger"
+	"github.com/xuperchain/xupercore/kernel/contract"
 	"github.com/xuperchain/xupercore/kernel/engines/xuperos/common"
 	"github.com/xuperchain/xupercore/lib/logs"
 )
@@ -27,12 +29,35 @@ func (t *LedgerAgent) GetCryptoType() (int, error) {
 	return 0, nil
 }
 
-// 获取状态机最新确认高度快照
+// 查询区块
+func (t *LedgerAgent) QueryBlock(blkId []byte) (*BlockAgent, error) {
+	return nil, nil
+}
+
+func (t *LedgerAgent) QueryBlockByHeight(int64) (*BlockAgent, error) {
+	return nil, nil
+}
+
+func (t *LedgerAgent) GetTipBlock() *BlockAgent {
+
+}
+
+// 获取状态机最新确认高度快照（只有Get方法，直接返回[]byte）
 func (t *LedgerAgent) GetTipXMSnapshotReader() (ledger.XMSnapshotReader, error) {
 	return nil, nil
 }
 
-// 查询区块
-func (t *LedgerAgent) QueryBlock(blkId []byte) (BlockInterface, error) {
+// 根据指定blockid创建快照（Select方法不可用）
+func (t *LedgerAgent) CreateSnapshot(blkId []byte) (ledger.XMReader, error) {
+
+}
+
+// 获取最新确认高度快照（Select方法不可用）
+func (t *LedgerAgent) GetTipSnapshot() (ledger.XMReader, error) {
+
+}
+
+// 获取最新状态数据
+func (t *LedgerAgent) CreateXMReader() (ledger.XMReader, error) {
 
 }
--- kernel/ledger/def.go
+++ kernel/ledger/def.go
@@ -4,3 +4,32 @@ package ledger
 type XMSnapshotReader interface {
 	Get(bucket string, key []byte) ([]byte, error)
 }
+
+type XMReader interface {
+	//读取一个key的值，返回的value就是有版本的data
+	Get(bucket string, key []byte) (*VersionedData, error)
+	//扫描一个bucket中所有的kv, 调用者可以设置key区间[startKey, endKey)
+	Select(bucket string, startKey []byte, endKey []byte) (XMIterator, error)
+}
+
+// XMIterator iterates over key/value pairs in key order
+type XMIterator interface {
+	Key() []byte
+	Value() *VersionedData
+	Next() bool
+	Error() error
+	// Iterator 必须在使用完毕后关闭
+	Close()
+}
+
+type PureData struct {
+	Bucket string
+	Key    []byte
+	Value  []byte
+}
+
+type VersionedData struct {
+	PureData  *PureData
+	RefTxid   []byte
+	RefOffset int32
+}
