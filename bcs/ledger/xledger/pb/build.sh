74b8f914c2cefc6d0d8cbbf0e7f729b845b05dea
chunhui01 <chunhui01@outlook.com>
2021-01-07 16:10:06 +0800

    update protos

--- bcs/ledger/xledger/pb/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-protoc -I ./ -I ../../../../protos/ --go_opt=paths=source_relative --go_out=plugins=grpc:./ ./xledger.proto
-
--- protos/build.sh
+++ protos/build.sh
@@ -3,7 +3,7 @@
 # protoc v3.7.1
 # protoc-gen-go v1.3.3
 
-protoc -I ./ ./network.proto --go_opt=paths=source_relative --go_out=plugins=grpc:./
-protoc -I ./ ./permission.proto --go_opt=paths=source_relative --go_out=plugins=grpc:./
-protoc -I ./ ./contract.proto --go_opt=paths=source_relative --go_out=plugins=grpc:./
-protoc -I ./ ./ledger.proto --go_opt=paths=source_relative --go_out=plugins=grpc:./
+protoc -I ./ -I ../../ ./network.proto --go_opt=paths=source_relative --go_out=plugins=grpc:./
+protoc -I ./ -I ../../ ./permission.proto --go_opt=paths=source_relative --go_out=plugins=grpc:./
+protoc -I ./ -I ../../ ./contract.proto --go_opt=paths=source_relative --go_out=plugins=grpc:./
+protoc -I ./ -I ../../ ./ledger.proto --go_opt=paths=source_relative --go_out=plugins=grpc:./
