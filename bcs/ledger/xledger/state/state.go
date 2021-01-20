87da42bba6eb5f3ef1dd4fd7957f36a2ab91870b
weizhimin <weizhimin@baidu.com>
2021-01-20 13:03:33 +0800

    fix walk

--- bcs/ledger/xledger/state/state.go
+++ bcs/ledger/xledger/state/state.go
@@ -523,9 +523,10 @@ func (t *State) RollBackUnconfirmedTx() (map[string]bool, []*pb.Transaction, err
 		return nil, nil, writeErr
 	}
 
-	// 回滚完成从未确认交易表删除
-	for txid := range undoDone {
-		t.tx.UnconfirmTxInMem.Delete(txid)
+	// 由于这里操作不是原子操作，需要保持按回滚顺序delete
+	for _, tx := range undoList {
+		t.tx.UnconfirmTxInMem.Delete(tx.Txid)
+		t.log.Trace("delete from unconfirm tx memory", "txid", utils.F(tx.Txid))
 	}
 	return undoDone, undoList, nil
 }
