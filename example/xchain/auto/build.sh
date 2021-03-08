b4d20ddb98ffd429b938acb07b82bb3a8afb8cee
wanchunhui <chunhui01@outlook.com>
2021-03-08 10:33:11 +0800

    support xvm compilation (#150)
    support xvm compilation.

--- example/xchain/auto/build.sh
+++ example/xchain/auto/build.sh
@@ -4,12 +4,19 @@ cd `dirname $0`/../../../
 
 HOMEDIR=`pwd`
 OUTDIR="$HOMEDIR/output"
+XVMDIR="$HOMEDIR/.compile_cache/xvm"
 
 # make output dir
 if [ ! -d "$OUTDIR" ];then
     mkdir $OUTDIR
 fi
 rm -rf "$OUTDIR/*"
+
+# check xvm
+if [ ! -f "$XVMDIR/wasm2c" ];then
+    echo "please first execute: make xvm"
+    exit 1
+fi
 
 function buildpkg() {
     output=$1
@@ -49,5 +56,6 @@ cp -r "$HOMEDIR/example/xchain/conf" "$OUTDIR"
 cp "$HOMEDIR/example/xchain/auto/control.sh" "$OUTDIR"
 mkdir -p "$OUTDIR/data"
 cp -r "$HOMEDIR/example/xchain/data/genesis" "$OUTDIR/data"
+cp "$XVMDIR/wasm2c" "$OUTDIR/bin"
 
 echo "compile done!"
