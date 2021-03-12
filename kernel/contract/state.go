3854ce524ab9fd942d3b748bb74efec50c23ddf0
fanbingxin <fanbingxin.me@gmail.com>
2021-03-12 19:30:14 +0800

    kernel/contract: add contract event (#158)

--- kernel/contract/state.go
+++ kernel/contract/state.go
@@ -2,26 +2,8 @@ package contract
 
 import (
 	"github.com/xuperchain/xupercore/kernel/ledger"
+	"github.com/xuperchain/xupercore/protos"
 )
-
-// // XMIterator iterates over key/value pairs in key order
-// type XMIterator interface {
-// 	Key() []byte
-// 	Value() *VersionedData
-// 	Next() bool
-// 	Error() error
-// 	// Iterator 必须在使用完毕后关闭
-// 	Close()
-// }
-
-// // XMReader 为账本的XModel的读接口集合，
-// // 合约通过XMReader构造StateSandbox，从而生成读写集
-// type XMReader interface {
-// 	//读取一个key的值，返回的value就是有版本的data
-// 	Get(bucket string, key []byte) (*VersionedData, error)
-// 	//扫描一个bucket中所有的kv, 调用者可以设置key区间[startKey, endKey)
-// 	Select(bucket string, startKey []byte, endKey []byte) (XMIterator, error)
-// }
 
 type SandboxConfig struct {
 	XMReader ledger.XMReader
@@ -55,30 +37,26 @@ type UTXOState interface {
 type CrossQueryState interface {
 }
 
+type ContractEventState interface {
+	AddEvent(events ...*protos.ContractEvent)
+}
+
 // State 抽象了链的状态机接口，合约通过State里面的方法来修改状态。
 type State interface {
 	XMState
 	UTXOState
 	CrossQueryState
+	ContractEventState
 }
 
 // StateSandbox 在沙盒环境里面执行状态修改操作，最终生成读写集
 type StateSandbox interface {
 	State
+	// Flush将缓存的UTXO，CrossQuery等内存状态写入到读写集
+	// 没有调用Flush只能得到KV数据的读写集
+	Flush() error
 	RWSet() *RWSet
 }
-
-// type PureData struct {
-// 	Bucket string
-// 	Key    []byte
-// 	Value  []byte
-// }
-
-// type VersionedData struct {
-// 	PureData  *PureData
-// 	RefTxid   []byte
-// 	RefOffset int32
-// }
 
 type RWSet struct {
 	RSet []*ledger.VersionedData
