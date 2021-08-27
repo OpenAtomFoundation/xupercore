c19b6f1a2a95b01343ff8d54cf5ce1e45c426e19
fanbingxin <fanbingxin.me@gmail.com>
2021-08-27 16:11:02 +0800

    miner: new method of sync blocks for miner (#235)

--- kernel/common/xcontext/base_ctx.go
+++ kernel/common/xcontext/base_ctx.go
@@ -18,8 +18,17 @@ type XContext interface {
 // 考虑到后续对操作生命周期做控制，先实现空context.Context接口，方便扩展
 // 同时定义扩展全局需要的公共成员，方便为各对象统一注入和管理
 type BaseCtx struct {
+	context.Context
 	XLog  logs.Logger
 	Timer *timer.XTimer
+}
+
+func WithNewContext(parent XContext, ctx context.Context) XContext {
+	return &BaseCtx{
+		Context: ctx,
+		XLog:    parent.GetLog(),
+		Timer:   parent.GetTimer(),
+	}
 }
 
 func (t *BaseCtx) GetLog() logs.Logger {
@@ -31,17 +40,29 @@ func (t *BaseCtx) GetTimer() *timer.XTimer {
 }
 
 func (t *BaseCtx) Deadline() (deadline time.Time, ok bool) {
+	if t.Context != nil {
+		return t.Context.Deadline()
+	}
 	return
 }
 
 func (t *BaseCtx) Done() <-chan struct{} {
+	if t.Context != nil {
+		return t.Context.Done()
+	}
 	return nil
 }
 
 func (t *BaseCtx) Err() error {
+	if t.Context != nil {
+		t.Context.Err()
+	}
 	return nil
 }
 
 func (t *BaseCtx) Value(key interface{}) interface{} {
+	if t.Context != nil {
+		return t.Context.Value(key)
+	}
 	return nil
 }
