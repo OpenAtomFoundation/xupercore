2ee4d0abe795f15c6eab3ca6ebec2828ff7fcb99
Go语言之美 <122943094@qq.com>
2021-10-29 15:26:51 +0800

    Cherrypick mempool tx limit and consensus (#281)
    mempool tx limti and consensus.

--- bcs/ledger/xledger/config/config.go
+++ bcs/ledger/xledger/config/config.go
@@ -16,6 +16,7 @@ type XLedgerConf struct {
 	Utxo           UtxoConfig `yaml:"utxo,omitempty"`
 	BlockCacheSize int        `yaml:"blockCacheSize,omitempty"`
 	TxCacheSize    int        `yaml:"txCacheSize,omitempty"`
+	MempoolTxLimit int        `yaml:"mempoolTxLimit,omitempty"`
 }
 
 type UtxoConfig struct {
