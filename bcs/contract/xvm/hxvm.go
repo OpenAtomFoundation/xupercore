782d3490cd030d900d09b03b40d2d3e2d3975313
chenfengjin <chenfengjin@baidu.com>
2022-05-11 18:29:24 +0800

    add hybrid implementation for webassembly contract

--- bcs/contract/xvm/hxvm.go
+++ bcs/contract/xvm/hxvm.go
@@ -1,72 +1,103 @@
 package xvm
 
 import (
-	"time"
+	"path/filepath"
 
 	"github.com/xuperchain/xupercore/kernel/contract"
 	"github.com/xuperchain/xupercore/kernel/contract/bridge"
 )
 
 type HXVMCreator struct {
-	creator  bridge.InstanceCreator
-	lastSeen time.Time
-	count    int64
-	config   *bridge.InstanceCreatorConfig
+	tier0Creator *xvmInterpCreator
+	tier1Creator *xvmCreator
+	tier2Creator *xvmCreator
 }
 
+// TODO @chenfengjin
+// 1. 添加 tier up 时的错误处理
+// 2. 避免重复编译，以节省计算资源
+// 3. 增加对应的 prometheus metric
 func newHXVMCreator(creatorConfig *bridge.InstanceCreatorConfig) (bridge.InstanceCreator, error) {
-	ixvm, err := newXVMInterpCreator(creatorConfig)
-	if err != nil {
-		return nil, err
-	}
-	creator := &HXVMCreator{
-		creator: ixvm,
-	}
-	go creator.tierUp1()
-	return creator, nil
-}
+	baseDir := creatorConfig.Basedir
 
-func (creator *HXVMCreator) tierUp1() {
-	config := creator.config
-	aotxvm, err := newXVMCreator(creator.config)
-	config.VMConfig = &contract.WasmConfig{
+	tier0Config := *creatorConfig
+	tier1Config := *creatorConfig
+	tier2Config := *creatorConfig
+
+	tier0Config.Basedir = filepath.Join(baseDir, "tier0")
+	tier1Config.Basedir = filepath.Join(baseDir, "tier1")
+	tier2Config.Basedir = filepath.Join(baseDir, "tier2")
+
+	tier1Config.VMConfig = &contract.WasmConfig{
 		XVM: contract.XVMConfig{
 			OptLevel: 0,
 		},
 	}
-	if err != nil {
-
-	}
-	creator.creator = aotxvm
-}
-
-func (creator *HXVMCreator) tierUp2() {
-	config := creator.config
-	config.VMConfig = &contract.WasmConfig{
+	tier2Config.VMConfig = &contract.WasmConfig{
 		XVM: contract.XVMConfig{
-			OptLevel: 3,
+			OptLevel: 2,
 		},
 	}
-	aotxvm, err := newXVMCreator(creator.config)
+
+	tier0Creator, err := newXVMInterpCreator(&tier0Config)
 	if err != nil {
-
+		return nil, err
 	}
-	creator.creator = aotxvm
+	tier1Creator, err := newXVMCreator(&tier1Config)
+	if err != nil {
+		return nil, err
+	}
+
+	tier2Creator, err := newXVMCreator(&tier2Config)
+	if err != nil {
+		return nil, err
+	}
+
+	creator := &HXVMCreator{
+		tier0Creator: tier0Creator.(*xvmInterpCreator),
+		tier1Creator: tier1Creator.(*xvmCreator),
+		tier2Creator: tier2Creator.(*xvmCreator),
+	}
+	return creator, nil
 }
 
-func (vm *HXVMCreator) CreateInstance(ctx *bridge.Context, cp bridge.ContractCodeProvider) (bridge.Instance, error) {
-	if time.Now().Sub(vm.lastSeen) < time.Minute {
-		if vm.count > 10 {
-			go vm.tierUp2()
-		}
-	} else {
-		vm.lastSeen = time.Now()
-	}
-	return vm.creator.CreateInstance(ctx, cp)
+func (creator *HXVMCreator) tierUp1(ctx *bridge.Context, cp bridge.ContractCodeProvider) {
+	creator.tier1Creator.CreateInstance(ctx, cp)
 }
 
-func (vm *HXVMCreator) RemoveCache(name string) {
-	vm.creator.RemoveCache(name)
+func (creator *HXVMCreator) tierUp2(ctx *bridge.Context, cp bridge.ContractCodeProvider) {
+	creator.tier2Creator.CreateInstance(ctx, cp)
+}
+
+func (creator *HXVMCreator) CreateInstance(ctx *bridge.Context, cp bridge.ContractCodeProvider) (bridge.Instance, error) {
+	codeDesc, err := cp.GetContractCodeDesc(ctx.ContractName)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, find := creator.tier2Creator.cm.lookupDiskCache(ctx.ContractName, codeDesc); find {
+		return creator.tier2Creator.CreateInstance(ctx, cp)
+	}
+
+	if _, find := creator.tier1Creator.cm.lookupDiskCache(ctx.ContractName, codeDesc); find {
+		go creator.tierUp2(ctx, cp)
+		return creator.tier1Creator.CreateInstance(ctx, cp)
+	}
+
+	instance, err := creator.tier0Creator.CreateInstance(ctx, cp)
+	if err != nil {
+		return nil, err
+	}
+
+	go creator.tierUp1(ctx, cp)
+	return instance, err
+
+}
+
+func (creator *HXVMCreator) RemoveCache(name string) {
+	creator.tier0Creator.RemoveCache(name)
+	creator.tier1Creator.RemoveCache(name)
+	creator.tier2Creator.RemoveCache(name)
 }
 
 func init() {
